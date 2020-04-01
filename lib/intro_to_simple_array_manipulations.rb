ec871e3b5c739ef2bfe9c5eb45e06caaf30d1e79
Marcelo Bini <marcelobini@gmail.com>
2020-04-01 20:06:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,10 +10,15 @@ def using_concat(my_favorite_things, more_favs)
    @new_array = @list_of_esoteric_programming_languages.insert(4, @another_esoteric_language)
  end
 
-  def using_uniq(captain_planet_and_the_planeteers)
-   captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
-   new_array = captain_planet_and_the_planeteers.uniq
+  def using_uniq(haircuts)
+   captain_planet_and_the_planeteers = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+   new_array = haircuts.uniq
  end
+ #describe "using_uniq" do 
+   #it "takes in an argument of an array and uses the uniq method to remove any duplicate items" do
+     #haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+     #new_array = using_uniq(haircuts)
+     #expect(new_array).to eq(["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler"])
 
   def using_flatten(instruments)
    instruments = ["Saxophone", "Piano", "Trumpet", "Violin", "Drums", "Flute"]
